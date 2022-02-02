eae3b31420205b35610c7944660e4a987ead2bb1
Zhannur <jannuraidynuly@gmail.com>
2022-02-02 13:15:28 +0500

    global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function start() {
     const PORT = process.env.PORT || 5001;
     const app = await NestFactory.create(AppModule)
+
+    app.useGlobalPipes(new ValidationPipe())
 
     await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
--- src/users/dto/create-user-dto.ts
+++ src/users/dto/create-user-dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, Length } from "class-validator";
+
 export class CreateUserDto {
+    @IsEmail({}, {message: 'Некорректный email'})
     readonly email: string;
+
+    @Length(4, 16, {message: 'Не меньше 4 и не больше 16'})
     readonly password: string;
 }
